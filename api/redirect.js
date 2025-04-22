df33d227ba866adc1121dea760b8f0f3d5b60f08
Nahiyaan Rahman Shaan <134259658+itzshaan@users.noreply.github.com>
2025-04-22 10:52:49 +0600

    redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -9,21 +9,21 @@ export default function handler(req, res) {
     const decodedImage = Buffer.from(image, 'base64').toString('ascii');
     const decodedUrl = Buffer.from(url, 'base64').toString('ascii');
 
-    // HTML with Open Graph tags for Facebook
+    // HTML with Open Graph tags
     const html = `
     <!DOCTYPE html>
     <html prefix="og: https://ogp.me/ns#">
     <head>
-        <meta property="og:title" content="Check this out!">
+        <meta property="og:title" content="watch now">
         <meta property="og:image" content="${decodedImage}">
-        <meta property="og:description" content="Amazing offer">
+        <meta property="og:image:width" content="1200">
+        <meta property="og:image:height" content="630">
+        <meta property="og:description" content="Click to view">
         <meta property="og:url" content="${decodedUrl}">
         <meta http-equiv="refresh" content="0; url=${decodedUrl}">
     </head>
     <body>
-        <script>
-            window.location.href = "${decodedUrl}";
-        </script>
+        Redirecting...
     </body>
     </html>
     `;
